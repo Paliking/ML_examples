1e588fc210938906c204882116b2433b391f131c
Paliking <pablo.nicitel@gmail.com>
2016-06-23 15:51:36 +0200

    LogReg added

--- LoanPrediction/LoanPrediction.py
+++ LoanPrediction/LoanPrediction.py
@@ -11,9 +11,105 @@ zdroj: http://www.analyticsvidhya.com/blog/2016/01/complete-tutorial-learn-data-
 
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import scipy as sp
+import matplotlib.pyplot as plt
 
-df = pd.read_csv("train_u6lujuX_CVtuZ9i.csv")
+from sklearn.preprocessing import LabelEncoder
+from sklearn.cross_validation import KFold   #For K-fold cross validation
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn import metrics
+
+from sklearn.cross_validation import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+
+from matplotlib.colors import ListedColormap
+cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+cm = plt.cm.RdBu
+cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+
+
+
+
+'''
+- split na train a test set
+- gridsearch
+- vysledok je best estimator a vypise best skore
+'''
+def cv_optimize(clf, parameters, Xtrain, ytrain, n_folds=5):
+    gs = GridSearchCV(clf, param_grid=parameters, cv=n_folds)
+    gs.fit(Xtrain, ytrain)
+    print ("BEST PARAMS", gs.best_params_)
+    best = gs.best_estimator_
+    return best
+
+
+
+
+'''
+Funkcia na tunovanie parametrov. Data sa delia. na trainsete sa robi 5 fold CV.
+Najlepsi parameter je potom pouzity na celom training sete.
+Vypise sa Accuracy na training a testing sete
+
+
+VSTUPY
+clf - clasifier
+parameters - na hladanie v gridsearchu
+indf - dataframe s featurmi a targetom spolu
+featurenames - list feature nazvov v dataframe, kt. sa maju pouzit
+targetname - list target nazvov v dataframe
+standardize - ci sa maju normalizovat data
+train_size - kolko % ma byt training setu
+
+OUTPUT
+clf - najlepsi clasifier fitovany na celom training sete
+Xtrain, ytrain, Xtest, ytest - X a y training a testing setu
+'''
+def do_classify(clf, parameters, indf, featurenames, targetname, standardize=False, train_size=0.7):
+    subdf=indf[featurenames]
+    if standardize:
+        subdfstd=(subdf - subdf.mean())/subdf.std()
+    else:
+        subdfstd=subdf
+    X=subdfstd.values
+    y=indf[targetname].values
+    Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, train_size=train_size)
+    clf = cv_optimize(clf, parameters, Xtrain, ytrain)
+    clf=clf.fit(Xtrain, ytrain)
+    training_accuracy = clf.score(Xtrain, ytrain)
+    test_accuracy = clf.score(Xtest, ytest)
+    print ("Accuracy on training data: %0.2f" % (training_accuracy))
+    print ("Accuracy on test data:     %0.2f" % (test_accuracy))
+    return clf, Xtrain, ytrain, Xtest, ytest
+
+
+def train_best(clf, parameters, indf, featurenames, targetname, standardize=False):
+    subdf=indf[featurenames]
+    if standardize:
+        subdfstd=(subdf - subdf.mean())/subdf.std()
+    else:
+        subdfstd=subdf
+    X=subdfstd.values
+    y=indf[targetname].values
+    clf = cv_optimize(clf, parameters, X, y)
+    clf=clf.fit(X, y)
+    training_accuracy = clf.score(X, y)
+##    test_accuracy = clf.score(Xtest, ytest)
+    print ("Accuracy on training data: %0.2f" % (training_accuracy))
+##    print ("Accuracy on test data:     %0.2f" % (test_accuracy))
+    return clf
+
+
+train = pd.read_csv("train_u6lujuX_CVtuZ9i.csv")
+test = pd.read_csv("test_Y3wMUE5_7gLdaTN.csv")
+train['source']='train'
+test['source']='test'
+
+df = pd.concat([train, test], ignore_index=True)
 
 print(df.describe())
 
@@ -22,9 +118,121 @@ df['Property_Area'].value_counts()
 
 
 #  study distribution of various variables
-df['ApplicantIncome'].hist(bins=50)
-df.boxplot(column='ApplicantIncome')
-df.boxplot(column='ApplicantIncome', by = 'Education')
-
+##df['ApplicantIncome'].hist(bins=50)
+##plt.show()
+##df.boxplot(column='ApplicantIncome')
+##df.boxplot(column='ApplicantIncome', by = 'Education')
+##plt.show()
 
 df['Loan_Status'] = df['Loan_Status'].apply(lambda x: 1 if x=='Y' else 0)
+
+# percenta ludi co dostali pozicku ked mali/nemali Credit_History
+##print(df.pivot_table(index='Credit_History', values='Loan_Status' ))
+
+# alternativa v jednom kroku (z Y/N na 1/0 + pivottable)
+##df.pivot_table(values='Loan_Status',index=['Credit_History'],aggfunc=lambda x: x.map({'Y':1,'N':0}).mean())
+
+#----------------------------Data Munging -------------------------------------------------
+#----------fill missing values--------------
+# kontrola missing values
+missed = df.apply(lambda x: sum(x.isnull()),axis=0)
+print(missed)
+
+# nahradenie NaN priemerom
+##df['LoanAmount'].fillna(df['LoanAmount'].mean(), inplace=True)
+
+# anmiesto NaN v LoanAmount idem doplnit mediany podla LoanAmount vo Education a Self_Employed, najprv ale doplnim NaN v nich.
+
+# kedze vacsina ludi nieje Self_Employed, tak za Nan doplnim NO
+##df['Self_Employed'].value_counts()
+df['Self_Employed'].fillna('No',inplace=True)
+
+
+# median LoanAmountu podla Self_Employed Education
+table = df.pivot_table(values='LoanAmount', index='Self_Employed' ,columns='Education', aggfunc=np.median)
+# Define function to return value of this pivot_table
+def fage(x):
+    return table.loc[x['Self_Employed'],x['Education']]
+
+# Replace missing values
+df['LoanAmount'].fillna(df[df['LoanAmount'].isnull()].apply(fage, axis=1), inplace=True)
+
+
+# ideme sledovat frekvenciu kategorii
+#Filter categorical variables
+categorical_columns = [x for x in df.dtypes.index if df.dtypes[x]=='object']
+#Exclude ID cols and source:
+categorical_columns = [x for x in categorical_columns if x not in ['Loan_ID']]
+#Print frequency of categories
+for col in categorical_columns:
+    print ('\nFrequency of Categories for varible %s'%col)
+    print (df[col].value_counts())
+
+
+
+# ako baseline vsade vlozime najcastejsiu hodnotu
+df['Gender'].fillna('Male',inplace=True)
+df['Married'].fillna('Yes',inplace=True)
+df['Dependents'].fillna('0',inplace=True)
+df['Loan_Amount_Term'].fillna(360,inplace=True)
+# volim podla vyslednej pozicky, kedze tam je velka korelacia
+df['Credit_History'].fillna(df[df['Credit_History'].isnull()]['Loan_Status'], inplace=True)
+
+
+# --------extreme values-----------
+# velke pozicky su mozne, tak namiesto vylucenia ako outlier rozdelenie zlogaritmujeme, aby sme znizili ich extremny vplyv.
+df['LoanAmount_log'] = np.log(df['LoanAmount'])
+
+# rozdelenie sa teraz podoba viac normalnemu
+##df['LoanAmount_log'].hist(bins=20)
+
+
+# pre income sme spravili sucet ApplicantIncome a CoapplicantIncome a zlogaritmovali
+df['TotalIncome'] = df['ApplicantIncome'] + df['CoapplicantIncome']
+df['TotalIncome_log'] = np.log(df['TotalIncome'])
+##df['LoanAmount_log'].hist(bins=20)
+##plt.show()
+
+
+# --------------------------feature engineering------------
+df['LoanTotalIncome_ratio_logs'] = df['LoanAmount_log']/df['TotalIncome_log']
+df['LoanTotalIncome_ratio'] = df['LoanAmount']/df['TotalIncome']
+df['paidMonthlyTotalIncome_ratio'] = df['LoanAmount']/df['Loan_Amount_Term']*1000/df['TotalIncome']
+df['paidMonthlyTotalIncome_ratio_logs'] = df['LoanAmount_log']/df['Loan_Amount_Term']*1000/df['TotalIncome_log']
+
+
+#----------------------------building model--------------------
+# categorical variables into numeric
+var_mod = ['Gender','Married','Dependents','Education','Self_Employed','Property_Area','Loan_Status']
+
+le = LabelEncoder()
+for i in var_mod:
+    df[i] = le.fit_transform(df[i])
+
+print(df.dtypes)
+
+df_train = df[ df['source']== 'train' ]
+df_test = df[ df['source']== 'test' ]
+
+clf = LogisticRegression()
+parameters = {"C": [0.01, 0.1, 1, 10, 100]}
+predictors = [ i for i in df.columns if not i in ['Loan_ID', 'Loan_Status', 'LoanTotalIncome_ratio', 'paidMonthlyTotalIncome_ratio', 'source'] ]
+targetname = 'Loan_Status'
+bestcv, Xtrain, ytrain, Xtest, ytest = do_classify(clf, parameters, df_train, predictors, targetname, standardize=False, train_size=0.7)
+
+
+# trening 
+print('trening na celom sete')
+
+clf_best = train_best(clf, parameters, df_train, predictors, targetname, standardize=False)
+
+
+#Predict on testing data:
+df_test['target'] = clf_best.predict(df_test[predictors])
+df_test['target'] = df_test['target'].apply(lambda x: 'Y' if x==1 else 'N')
+
+submission = pd.DataFrame()
+submission['Loan_ID'] = df_test['Loan_ID']
+submission['Loan_Status'] = df_test['target']
+submission.to_csv('submission_LogReg.csv', index=False)
+
